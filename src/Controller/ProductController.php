6729ea2184ed476eb64157046023f144e5002113
JesusPCS <hazuki00@gmail.com>
2025-12-30 16:35:21 +0100

    form and view created FX

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -62,7 +62,7 @@ class ProductController extends AbstractController
             return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('product/edit.html.twig', [
+        return $this->renderForm('product/edit.html.twig', [
             'product' => $product,
             'form' => $form,
         ]);
